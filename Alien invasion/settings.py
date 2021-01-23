bee3a0750a34412b1eaa7b022abd61de0cebdfcd
George-Corfield <74022908+George-Corfield@users.noreply.github.com>
2021-01-23 16:59:49 +0000

    Update settings.py

--- Alien invasion/settings.py
+++ Alien invasion/settings.py
@@ -9,7 +9,7 @@ class Settings:
         self.ship_limit = 3
 
         #bullet settings
-        self.bullet_width = 800
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 3
